86853a913e2f63be17c52021b1a758bad22e0807
Brice Lenfant <brice@gizra.com>
2016-12-26 21:52:41 +0200

    Adding project report and user page.

---
+++ productivity/modules/custom/productivity_time_tracking/project_report/project_report.tpl.php
@@ -0,0 +1,38 @@
+<div class="row">
+  <div class="main-box clearfix project-box emerald-box col-sm-12">
+    <div class="main-box-body clearfix">
+      <p class="show-only-on-print"><?php print $date; ?></p>
+      <div id="header" class="col-sm-12">
+        <h1 id="project-title"><?php print t('Project Tracking Report'); ?></h1>
+        <h2 id="gizra-logo">gizra</h2>
+      </div>
+      <div id="search-filter" class="row">
+        <div class="col-sm-12 ">
+          <div class="col-sm-4"><p><?php print t('Project:'); ?> </p></div>
+          <div class="col-sm-4"><p><?php print t('Date:'); ?> </p></div>
+        </div>
+        <div class="col-sm-12">
+          <div class="col-sm-4">
+            <select id="project_filter" class="form-control">
+              <?php foreach($projects as $nid => $project_name): ?>
+                <option value="<?php print $nid;?>"<?php print ($nid == $current_project_id) ? 'selected' : ''; ?>><?php print $project_name; ?></option>
+              <?php endforeach; ?>
+            </select>
+          </div>
+          <div class="col-sm-4">
+            <input type="text" id="month" name="month" class="monthPicker form-control" />
+            <span class="add-on"><i class="icon-th"></i></span>
+          </div>
+          <div class="col-sm-4">
+            </div>
+            <button class="btn btn-primary apply" type="button">Apply</button>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-12"><?php print $message; ?></div>
+      </div>
+     <?php print $table; ?>
+    </div>
+  </div>
+</div>

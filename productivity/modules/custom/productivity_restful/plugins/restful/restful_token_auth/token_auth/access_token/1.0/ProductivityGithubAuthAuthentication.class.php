b2b97b3a633de267d2f23907c60eaee78032d701
Aron Novak <aron@novaak.net>
2017-02-16 11:58:31 +0100

    productivity_restful: fixed http req invocation, arguments were not according to signature

--- productivity/modules/custom/productivity_restful/plugins/restful/restful_token_auth/token_auth/access_token/1.0/ProductivityGithubAuthAuthentication.class.php
+++ productivity/modules/custom/productivity_restful/plugins/restful/restful_token_auth/token_auth/access_token/1.0/ProductivityGithubAuthAuthentication.class.php
@@ -173,7 +173,7 @@ class ProductivityGithubAuthAuthentication extends \RestfulAccessTokenAuthentica
    */
   protected function checkMembership($data, $options) {
     try {
-      productivity_github_http_request('orgs/' . variable_get('productivity_github_organization', 'Gizra') . '/members/' . $data['login'], FALSE);
+      productivity_github_http_request('orgs/' . variable_get('productivity_github_organization', 'Gizra') . '/members/' . $data['login'], array(), FALSE);
     }
     catch (RestfulException $e) {
       // https://developer.github.com/v3/orgs/members/#check-membership

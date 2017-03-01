91718a5d471c9fb23f75aab63ad68160283d7fc1
Brice Lenfant <brice@gizra.com>
2017-03-01 09:40:38 +0200

    Dynamic form.

--- productivity/modules/custom/productivity_user/plugins/content_types/tracking_form/tracking-form.tpl.php
+++ productivity/modules/custom/productivity_user/plugins/content_types/tracking_form/tracking-form.tpl.php
@@ -1,118 +1,165 @@
 <h2>{{ month }} - {{ year }}</h2>
-<div class="row">
-  <div class="col-md-10 col-md-push-1 col-md-pull-1">
-    <form class="form-horizontal tracking-form" role="form">
+<div class="row clearfix">
+  <div class="col-md-12 column">
+    <table class="table table-striped table-hover track-item-table table-condensed">
+      <thead>
+      <tr>
+        <th>#</th>
+        <?php for($i=1; $i<=$last_day; $i++): ?>
+          <th><?php print $i; ?></th>
+        <?php endfor; ?>
+      </tr>
+      </thead>
+      <tbody>
+      <tr class="track-item">
+        <?php foreach($tracking as $tracks): ?>
+          <td>
+            <ul class="list-unstyled">
+              <?php foreach($tracks as $track): ?>
+                <li>
+                  <a href=" <?php print $track['href']; ?>" data-toggle="tooltip" title="<?php print $track['title']; ?>" class=" <?php print $track['type']; ?>">
+                    <?php print $track['length']; ?>
+                  </a>
+                </li>
+              <?php endforeach; ?>
+            </ul>
+          </td>
+        <?php endforeach; ?>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
+
+<div class="row clearfix">
+  <div class="col-md-12 column">
+    <form class="form-horizontal">
       <fieldset>
+
         <!-- Form Name -->
         <legend>Tracking for 1/1/2017</legend>
 
-        <!-- Day type (inline) -->
+        <!-- Select Basic -->
         <div class="form-group">
-          <label class="col-md-2 control-label">Day type</label>
-          <div class="col-md-10">
-            <label class="radio-inline" for="daytype-0">
-              <input type="radio" value="regular" checked="checked">
-              Regular
-            </label>
-            <label class="radio-inline" for="daytype-1">
-              <input type="radio" value="sick">
-              Sick
-            </label>
-            <label class="radio-inline" for="daytype-2">
-              <input type="radio" value="vacation">
-              Vacation
-            </label>
-            <label class="radio-inline" for="daytype-3">
-              <input type="radio" value="miluim">
-              Miluim
-            </label>
-            <label class="radio-inline" for="daytype-4">
-              <input type="radio" value="convention">
-              Convention
-            </label>
+          <label class="col-md-4 control-label" for="selectbasic">Day type</label>
+          <div class="col-md-4">
+            <select id="selectbasic" name="selectbasic" class="form-control">
+              <option value="regular">Regular</option>
+              <option value="sick">Sick</option>
+              <option value="vacation">Vacation</option>
+              <option value="miluim">Miluim</option>
+              <option value="convention">Convention</option>
+            </select>
           </div>
         </div>
-
-        <div class="regular-day-wrapper">
-          <!-- Select Project -->
-          <div class="form-group">
-            <label class="col-md-2 col-xs-12 control-label" for="project">Project</label>
-            <div class="col-md-8 col-xs-10">
-              <select id="project" name="project" class="form-control">
-              </select>
-            </div>
-            <div class="col-md-2 col-xs-2">
-              <a class="btn btn-primary"><i class="fa fa-refresh"></i></a>
-            </div>
-          </div>
-
-          <!-- Issues tracker -->
-          <div class="form-group">
-            <label class="col-md-2 col-xs-12 control-label">Issues</label>
-            <div class="col-md-8 col-xs-12">
-              <div class="row issues-row">
-                <div class="col-md-6 col-xs-6">
-                  <input name="issue-label" type="text" required="true" placeholder="#1: example" class="form-control input-md issue-label">
-                </div>
-                <div class="col-md-3 col-xs-3">
-                  <select name="issues-hours" class="form-control" id="issue-type">
-                  </select>
-                </div>
-                <div class="col-md-3 col-xs-3">
-                  <input name="issue-time" type="number" step="0.10" min="0" placeholder="4" class="form-control input-md issue-time">
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <!-- Total hours-->
-        <div class="form-group">
-          <label class="col-md-2 control-label">Total hours</label>
-          <div class="col-md-8 total-hours">
-            <span>8</span>
-          </div>
-        </div>
-
-        <!-- Button -->
-        <div class="form-group">
-          <label class="col-md-2 control-label" for="submit"></label>
-          <div class="col-md-10">
-            <button id="submit" name="submit" type="submit" class="btn btn-primary"><i class="fa fa-check"></i> Save</button>
-            <a class="btn btn-success"><i class="fa fa-plus-circle"></i> New</a>
-            <a class="btn btn-danger"><i class="fa fa-times"></i> Remove</a>
-          </div>
-        </div>
-
-
       </fieldset>
     </form>
+
+    <table class="table table-bordered table-hover" id="tab_logic">
+      <thead>
+      <tr >
+        <th class="text-center">
+          #
+        </th>
+        <th class="text-center">
+          Project
+        </th>
+        <th class="text-center">
+          Issues #
+        </th>
+        <th class="text-center">
+          PR #
+        </th>
+        <th class="text-center">
+          Description
+        </th>
+        <th class="text-center">
+          Time (0)
+        </th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr id='addr0'>
+        <td>
+          1
+        </td>
+        <td>
+          <select id="selectbasic" name="selectbasic" class="form-control">
+            <option value="regular">Regular</option>
+            <option value="sick">Sick</option>
+            <option value="vacation">Vacation</option>
+            <option value="miluim">Miluim</option>
+            <option value="convention">Convention</option>
+          </select>
+        </td>
+        <td>
+          <input type="text" name='mail0' placeholder='#' class="form-control"/>
+        </td>
+        <td>
+          <input type="text" name='mail0' placeholder='#' class="form-control"/>
+        </td>
+        <td>
+          <input type="text" name='mobile0' placeholder='Description' class="form-control" disabled/>
+        </td>
+        <td>
+          <input name="issue-time" type="number" step="0.10" min="0" placeholder="4" class="form-control input-md issue-time">
+        </td>
+      </tr>
+      </tbody>
+    </table>
   </div>
 </div>
-<table class="table table-striped table-hover track-item-table table-condensed">
-  <thead>
-  <tr>
-    <th>#</th>
-    <?php for($i=1; $i<=$last_day; $i++): ?>
-      <th><?php print $i; ?></th>
-    <?php endfor; ?>
-  </tr>
-  </thead>
+
+<a id="add_row" class="btn btn-success"><i class="fa fa-plus-circle"></i> Add Row</a>
+<a id='delete_row' class="pull-right btn btn-default"><i class="fa fa-minus-circle"></i> Delete Row</a>
+<button id="submit" name="submit" type="submit" class="pull-left btn btn-primary"><i class="fa fa-check"></i> Save</button>
+
+<script>
+
+jQuery(document).ready(function(){
+  var i=1;
+  jQuery("#add_row").click(function(){
+    jQuery('#tab_logic').append(jQuery("#templateRow").clone().attr('id', 'addr'+(i)));
+    i++;
+  });
+  jQuery("#delete_row").click(function(){
+    if(i>1){
+      jQuery("#addr"+(i-1)).remove();
+      i--;
+    }
+  });
+
+});
+
+</script>
+
+<table style="display: none;">
   <tbody>
-  <tr class="track-item">
-    <?php foreach($tracking as $tracks): ?>
-    <td>
-      <ul class="list-unstyled">
-      <?php foreach($tracks as $track): ?>
-        <li>
-          <a href=" <?php print $track['href']; ?>" data-toggle="tooltip" title="<?php print $track['title']; ?>" class=" <?php print $track['type']; ?>">
-            <?php print $track['length']; ?>
-          </a>
-        </li>
-      <?php endforeach; ?>
-      </ul>
-    </td>
-    <?php endforeach; ?>
-  </tr>
+    <tr id="templateRow">
+      <td>
+        1
+      </td>
+      <td>
+        <select id="selectbasic" name="selectbasic" class="form-control">
+          <option value="regular">Regular</option>
+          <option value="sick">Sick</option>
+          <option value="vacation">Vacation</option>
+          <option value="miluim">Miluim</option>
+          <option value="convention">Convention</option>
+        </select>
+      </td>
+      <td>
+        <input type="text" name='mail0' placeholder='#' class="form-control"/>
+      </td>
+      <td>
+        <input type="text" name='mail0' placeholder='#' class="form-control"/>
+      </td>
+      <td>
+        <input type="text" name='mobile0' placeholder='Description' class="form-control" disabled/>
+      </td>
+      <td>
+        <input name="issue-time" type="number" step="0.10" min="0" placeholder="4" class="form-control input-md issue-time">
+      </td>
+    </tr>
   </tbody>
 </table>

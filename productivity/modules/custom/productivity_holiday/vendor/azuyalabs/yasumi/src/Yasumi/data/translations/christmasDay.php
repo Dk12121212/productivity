e98a571ce832ced59809be6daede955605ba167b
Aron Novak <aron@gizra.com>
2017-03-17 17:05:52 +0100

    productivity_holiday: start to implement dynamic holiday handling

---
+++ productivity/modules/custom/productivity_holiday/vendor/azuyalabs/yasumi/src/Yasumi/data/translations/christmasDay.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * This file is part of the Yasumi package.
+ *
+ * Copyright (c) 2015 - 2017 AzuyaLabs
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Sacha Telgenhof <stelgenhof@gmail.com>
+ */
+
+// Translations for Christmas
+return [
+    'cs_CZ' => '1. svátek vánoční',
+    'cy_GB' => 'Nadolig',
+    'da_DK' => '1. Juledag',
+    'de_AT' => 'Christtag',
+    'de_DE' => '1. Weihnachtsfeiertag',
+    'el_GR' => 'Χριστούγεννα',
+    'en_AU' => 'Christmas Day',
+    'en_GB' => 'Christmas Day',
+    'en_NZ' => 'Christmas Day',
+    'en_US' => 'Christmas',
+    'en_ZA' => 'Christmas Day',
+    'es_ES' => 'Navidad',
+    'fi_FI' => 'Joulupäivä',
+    'fr_FR' => 'Noël',
+    'hr_HR' => 'Božić',
+    'it_IT' => 'Natale',
+    'nb_NO' => 'Første juledag',
+    'nl_BE' => 'Kerstmis',
+    'nl_NL' => 'Kerstmis',
+    'pl_PL' => 'pierwszy dzień Bożego Narodzenia',
+    'pt_BR' => 'Natal',
+    'pt_PT' => 'Natal',
+    'ro_RO' => 'Crăciunul',
+    'ru_UA' => 'Рождество',
+    'sk_SK' => 'Prvý sviatok vianočný',
+    'sv_SE' => 'juldagen',
+    'uk_UA' => 'Різдво',
+];
---
+++ productivity/modules/custom/productivity_holiday/vendor/azuyalabs/yasumi/src/Yasumi/data/translations/immaculateConception.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This file is part of the Yasumi package.
+ *
+ * Copyright (c) 2015 - 2017 AzuyaLabs
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Sacha Telgenhof <stelgenhof@gmail.com>
+ */
+
+// Translations for Immaculate Conception
+return [
+    'de_AT' => 'Mariä Empfängnis',
+    'el_GR' => 'Ευαγγελισμός της Θεοτόκου',
+    'en_US' => 'Immaculate Conception',
+    'es_ES' => 'Inmaculada Concepción',
+    'it_IT' => 'Immacolata Concezione',
+    'pt_PT' => 'Dia da Imaculada Conceição',
+];

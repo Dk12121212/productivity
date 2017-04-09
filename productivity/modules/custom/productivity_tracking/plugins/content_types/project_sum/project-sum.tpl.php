f7b2e93ab07e4d8231bbef3637d489ca92a0a32c
Brice Lenfant <brice@gizra.com>
2017-04-09 16:31:37 +0300

    Sort user name and project name.

--- productivity/modules/custom/productivity_tracking/plugins/content_types/project_sum/project-sum.tpl.php
+++ productivity/modules/custom/productivity_tracking/plugins/content_types/project_sum/project-sum.tpl.php
@@ -3,10 +3,10 @@
     <div class="col-lg-6">
       <h3><span>Monthly project breakdown</span></h3>
       <ul class="list-group">
-        <?php foreach($tracking['sum']['projects'] as $project_nid => $data): ?>
+        <?php foreach($tracking['sum']['projects'] as $project_key => $data): ?>
           <li class="list-group-item">
             <span class="badge badge-primary"><?php print $data['sum']; ?></span>
-            <a href="<?php print url('node/' . $project_nid); ?>"><?php print $data['name']; ?></a>
+            <a href="<?php print url('node/' . $data['nid']); ?>"><?php print $data['name']; ?></a>
           </li>
         <?php endforeach; ?>
         <li class="list-group-item">

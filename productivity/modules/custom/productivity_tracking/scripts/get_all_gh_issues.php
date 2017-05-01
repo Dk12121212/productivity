7d4cc220b6daff0fe9ca1b7e1eb4c8b1cc0649ab
Brice Lenfant <brice@gizra.com>
2017-05-01 17:36:31 +0300

    Get all issue data script.

---
+++ productivity/modules/custom/productivity_tracking/scripts/get_all_gh_issues.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Fetch issues from GH and create or update tracking nodes.
+ */
+
+$repo_user = drush_get_option('gh_id', 'Gizra');
+$repo = drush_get_option('gh_id', 'ic-infuse');
+$min_issue_id = drush_get_option('min_issue_id', '1');
+$max_issue_id = drush_get_option('max_issue_id', '151');
+
+// TODO: deal with multiple project, in this case we don't want to assign issue
+// to new projects, we probably want to set a range per project using a other
+// option.
+
+// Add an option to not create new issues, just update.
+$just_update= drush_get_option('just_update', TRUE);
+
+// for test.
+$just_update = FALSE;
+
+// Disable issue caching.
+$no_cache = drush_get_option('no_cache', FALSE);
+
+for($issue_num = $min_issue_id; $issue_num <= $max_issue_id; $issue_num++) {
+  $issue_or_pr = productivity_tracking_get_issue_info($repo, $issue_num, $repo_user, TRUE, $no_cache);
+  $gh_username = '';
+  $pr = $issue = [];
+  drush_log("Processing $repo_user/$repo/$issue_num", 'success');
+  if(isset($issue_or_pr['issue']['pull_request'])) {
+    $pr = $issue_or_pr;
+    $gh_username = $issue_or_pr['issue']['user']['login'];
+    // Don't update PR for now. no need.
+    drush_log("Bypass PR $repo_user/$repo/$issue_num", 'success');
+    continue;
+  }
+  else {
+    $issue = $issue_or_pr;
+    // The user that opened the issue.
+    $gh_username = $issue_or_pr['issue']['user']['login'];
+  }
+  $repository_info = [];
+  $repository_info['full_name'] = "$repo_user/$repo";
+  if (!productivity_tracking_save_tracking($issue, $pr, $gh_username, $repository_info, $just_update, FALSE)) {
+    drush_log("Failed to update $repo_user/$repo/$issue_num  this might because the issue did not exist before.", 'error');
+  }
+  drush_log("Done $repo_user/$repo/$issue_num", 'success');
+}

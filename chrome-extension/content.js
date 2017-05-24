ab5e83cd36607e1d5120b748403730ef405390c9
amitaibu <amitai@gizra.com>
2017-05-24 14:20:19 +0300

    Get values from dom

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,2 +1,11 @@
-var el = document.querySelector('.TableObject-item--primary');
-el.insertAdjacentHTML('afterend', '<iframe src="http://productivity.gizra.com/" width="100%"></iframe>');
+var elRepoName = document.querySelector('#js-repo-pjax-container > div.pagehead.repohead.instapaper_ignore.readability-menu.experiment-repo-nav > div.container.repohead-details-container > h1 > strong > a');
+var repoName = elRepoName.getAttribute('href');
+
+var elIssueId = document.querySelector('.gh-header-number');
+var issueId = elIssueId.innerHTML.replace('#', '');
+
+console.log(repoName);
+console.log(issueId);
+
+var el = document.querySelector('.gh-header-meta');
+el.insertAdjacentHTML('afterend', '<iframe src="http://productivity.gizra.com/' + repoName + '/' + issueId + '" width="100%" height="100px"></iframe>');

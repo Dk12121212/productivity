bd62f14f4eb8fd9ce44114dbfd1d3c8ba36f1cb8
Amitai Burstein <amitai@gizra.com>
2017-05-25 00:29:50 +0300

    Set width

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -5,6 +5,9 @@ if (!location.ancestorOrigins.contains(extensionOrigin)) {
   // Must be declared at web_accessible_resources in manifest.json
   iframe.src = chrome.runtime.getURL('frame.html');
 
+  // Some styles for a fancy sidebar
+  iframe.style.cssText = 'width:100%;height:200px;';
+
   var el = document.querySelector('.gh-header-title');
   el.appendChild(iframe);
 }

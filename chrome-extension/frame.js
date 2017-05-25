fdd7ac44a765967682c32a5b2818f067c1fbff2d
Brice Lenfant <brice@gizra.com>
2017-05-26 00:02:00 +0300

    Cleanup

--- chrome-extension/frame.js
+++ chrome-extension/frame.js
@@ -2,6 +2,5 @@ var url = window.location.href
   .replace('/issues', '')
   .replace('https://github.com', '');
 
-// @todo: This will need to be HTTPS
  var url = 'https://productivity.gizra.com/' + repoName + '/' + issueId;
-document.querySelector('iframe').src = url;
+ document.querySelector('iframe').src = url;

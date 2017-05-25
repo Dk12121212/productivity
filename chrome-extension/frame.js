b0e6a5741673c2760c3fc58389a8b06cff5941d1
Brice Lenfant <brice@gizra.com>
2017-05-26 00:11:41 +0300

    Iframe url

--- chrome-extension/frame.js
+++ chrome-extension/frame.js
@@ -2,5 +2,6 @@ var url = window.location.href
   .replace('/issues', '')
   .replace('https://github.com', '');
 
- var url = 'https://productivity.gizra.com/' + repoName + '/' + issueId;
+ var url = 'https://productivity.gizra.com/per-issue/' + repoName + '/' + issueId;
+// var url = 'http://localhost/productivity/www/per-issue/' + repoName + '/' + issueId;
  document.querySelector('iframe').src = url;

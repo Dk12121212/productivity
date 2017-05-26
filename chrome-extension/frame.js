eaa128986b209125e7eaa682e5be61d3eb431219
Amitai Burstein <amitai@gizra.com>
2017-05-26 08:30:43 +0300

    Update url

--- chrome-extension/frame.js
+++ chrome-extension/frame.js
@@ -1,8 +1,6 @@
 var url = window.location.href
-  .replace('/issues', '')
-  .replace('https://github.com', '');
+  .replace('https://github.com', '')
+  .replace('/issues', '');
 
-// @todo: This will need to be HTTPS
-// var url = 'http://productivity.gizra.com/' + repoName + '/' + issueId;
-var url = "https://shoov.io/per-issue/" + url;
+var url = "https://productivity.gizra.com/per-issue/" + url;
 document.querySelector('iframe').src = url;

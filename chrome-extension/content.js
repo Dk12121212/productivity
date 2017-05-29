bc977e7f6bc2dedf25384ad4a1ee8714e9c28642
amitaibu <amitai@gizra.com>
2017-05-29 14:36:58 +0300

    Pass URL as query string to the child iframe
    fixes #431

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -2,8 +2,14 @@
 var extensionOrigin = 'chrome-extension://' + chrome.runtime.id;
 if (!location.ancestorOrigins.contains(extensionOrigin)) {
   var iframe = document.createElement('iframe');
+
+  // Pass the issue URL as query string to the parent IFrame.
+  var issueUrl = window.location.href
+    .replace('https://github.com/', '')
+    .replace('/issues', '');
+
   // Must be declared at web_accessible_resources in manifest.json
-  iframe.src = chrome.runtime.getURL('frame.html');
+  iframe.src = chrome.runtime.getURL('frame.html') + '?url=' + issueUrl;
 
   // Some styles for a fancy sidebar
   iframe.style.cssText = 'width:100%;height:200px;';

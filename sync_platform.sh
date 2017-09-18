3052ee89232256e1194317331368d9f858f617ab
Brice Lenfant <brice@gizra.com>
2017-09-18 15:19:06 +0300

    sync script.

--- sync_platform.sh
+++ sync_platform.sh
@@ -8,9 +8,9 @@
 #
 ################################################################################
 
-WORKING_DIR="/Users/brice/Sites/productivity/productivity"
-WORKING_DIR_app="/Users/brice/Sites/productivity/www/app/dist"
-PLATFORM_DIR="/Users/brice/Sites/productivity_pantheon"
+WORKING_DIR="/Applications/MAMP/htdocs/productivity/productivity"
+WORKING_DIR_app="/Applications/MAMP/htdocs/productivity/www/app/dist"
+PLATFORM_DIR="/Applications/MAMP/htdocs/productivity_pantheon"
 ALIAS="@productivity.dev"
 
 # Danel
@@ -18,12 +18,12 @@ rsync -azvr --delete-after --exclude=".git" --exclude=".idea" profiles/ $PLATFOR
 rsync -azvr --delete-after --exclude=".git" --exclude=".idea" $WORKING_DIR_app app/
 rsync -azvr --exclude=".git" --exclude=".gitignore" --delete-after profiles/productivity/composer $PLATFORM_DIR/sites/all
 
-
-cd $PLATFORM_DIR
-git pull
-git checkout master
-git add .
-git commit -am"Update site"
-git push
-drush $ALIAS uli
-cd $WORKING_DIR
+# 
+# cd $PLATFORM_DIR
+# git pull
+# git checkout master
+# git add .
+# git commit -am"Update site"
+# git push
+# drush $ALIAS uli
+# cd $WORKING_DIR

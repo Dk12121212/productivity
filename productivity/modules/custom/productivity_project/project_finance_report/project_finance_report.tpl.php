bccdae0e756feebd345627b4503c9e1916514e6e
Brice Lenfant <brice@gizra.com>
2017-10-19 11:52:21 +0300

    Finishing table view.

--- productivity/modules/custom/productivity_project/project_finance_report/project_finance_report.tpl.php
+++ productivity/modules/custom/productivity_project/project_finance_report/project_finance_report.tpl.php
@@ -4,10 +4,38 @@
       <div class="row">
         <div class="col-sm-12"><?php print $message; ?></div>
       </div>
-      <?php foreach($tables as $year => $table): ?>
-        <h3>Year <?php print $year; ?></h3>
-        <?php print $table; ?>
-      <?php endforeach; ?>
+      <h3>Month <?php print "$year/$month"; ?></h3>
+      <div class="row">
+        <div class="col-lg-3 col-sm-6 col-xs-12">
+          <div class="main-box infographic-box">
+            <i class="fa fa-user red-bg"></i>
+            <span class="headline">Total Hours</span>
+            <span class="value"><?php print $total_hours; ?></span>
+          </div>
+        </div>
+        <div class="col-lg-3 col-sm-6 col-xs-12">
+          <div class="main-box infographic-box">
+            <i class="fa fa-eye yellow-bg"></i>
+            <span class="headline">Total Hours not billed</span>
+            <span class="value"><?php print $total_hours_not_billed; ?></span>
+          </div>
+        </div>
+        <div class="col-lg-3 col-sm-6 col-xs-12">
+          <div class="main-box infographic-box">
+            <i class="fa fa-shopping-cart emerald-bg"></i>
+            <span class="headline">Total Projects</span>
+            <span class="value"><?php print $total_projects; ?></span>
+          </div>
+        </div>
+        <div class="col-lg-3 col-sm-6 col-xs-12">
+          <div class="main-box infographic-box">
+            <i class="fa fa-money green-bg"></i>
+            <span class="headline">Income</span>
+            <span class="value"><?php print $total_ils; ?></span>
+          </div>
+        </div>
+      </div>
+      <?php print $table; ?>
     </div>
   </div>
 </div>

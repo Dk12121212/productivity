ca242dfab16602a5e21a90772339da7b6f053869
Brice Lenfant <brice@gizra.com>
2017-11-30 12:41:45 +0200

    Fix layout.

--- productivity/themes/custom/bootstrap_subtheme/layouts/user/user.tpl.php
+++ productivity/themes/custom/bootstrap_subtheme/layouts/user/user.tpl.php
@@ -13,6 +13,7 @@
         <ul class="nav nav-tabs">
           <li class="<?php print (!isset($_GET['active_tab']) || ($_GET['active_tab'] == 'tracking')) ? 'active' : ''; ?>"><a href="#tab-tracking" data-toggle="tab">Tracking</a></li>
           <li class="<?php print (isset($_GET['active_tab']) && ($_GET['active_tab'] == 'timewatch')) ? 'active' : ''; ?>"><a href="#tab-attendence" data-toggle="tab">Timewatch</a></li>
+          <li class="<?php print (isset($_GET['active_tab']) && ($_GET['active_tab'] == 'totals')) ? 'active' : ''; ?>"><a href="#tab-totals" data-toggle="tab">Project totals</a></li>
         </ul>
         <div class="tab-content">
           <div class="tab-pane fade in <?php print (!isset($_GET['active_tab']) || ($_GET['active_tab'] == 'tracking')) ? 'active' : ''; ?>" id="tab-tracking">
@@ -20,6 +21,20 @@
           </div>
           <div class="tab-pane fade in <?php print (isset($_GET['active_tab']) && ($_GET['active_tab'] == 'timewatch')) ? 'active' : ''; ?>" id="tab-attendence">
             <?php print $content['attendance']; ?>
+            <div class="main-box-body emerald-box col-sm-6">
+              <?php print $content['attendance-left']; ?>
+            </div>
+            <div class="main-box-body emerald-box col-sm-6">
+              <?php print $content['attendance-right']; ?>
+            </div>
+          </div>
+          <div class="tab-pane fade in <?php print (isset($_GET['active_tab']) && ($_GET['active_tab'] == 'totals')) ? 'active' : ''; ?>" id="tab-totals">
+            <div class="main-box-body emerald-box col-sm-6">
+              <?php print $content['totals-left']; ?>
+            </div>
+            <div class="main-box-body emerald-box col-sm-6">
+              <?php print $content['totals-right']; ?>
+            </div>
           </div>
         </div>
       </div>

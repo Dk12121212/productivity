c50bab0b41f18249caa01415c0de178f4f9405be
Brice Lenfant <brice@gizra.com>
2017-11-30 11:55:50 +0200

    Creating plugin structure, and reformatting able query to support start and end day.

---
+++ productivity/modules/custom/productivity_tracking/plugins/content_types/project_sum_per_week/project-sum-week.tpl.php
@@ -0,0 +1,14 @@
+<div id="project-sum">
+  <ul class="list-group">
+    <?php foreach($tracking['sum']['projects'] as $project_key => $data): ?>
+      <li class="list-group-item">
+        <span class="badge badge-primary" nid="<?php print $data['nid']; ?>"><?php print $data['sum']; ?></span>
+        <a href="<?php print url('node/' . $data['nid']); ?>"><?php print $data['name']; ?></a>
+      </li>
+    <?php endforeach; ?>
+    <li class="list-group-item">
+      <span class="badge badge-danger"><?php print $tracking['sum']['month']; ?></span>
+      Monthly total
+    </li>
+  </ul>
+</div>
